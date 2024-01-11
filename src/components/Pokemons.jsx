7e5d414d10c2da8f04115704fca62a701540d454
Blackblur91 <feketebalazs04@gmail.com>
2024-01-11 11:52:30 +0100

    added mui

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -1,8 +1,13 @@
+import Button from '@mui/material/Button';
 import Pokemon from "./Pokemon"
+
+import './Pokemon.css'
+import { Card, CardHeader, CardMedia } from '@mui/material';
 
 function Pokemons({ pokemons }) {
 	return (
 		<div className="pokemons">
+			
 			{pokemons.map((pokemon, index) => <Pokemon key={index} pokemon={pokemon} />)}
 		</div>
 	)
